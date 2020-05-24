325a82023b707b4eb1fe83d712dcf1f56451d8be
Brittany Price <bprice0496@gmail.com>
2020-05-23 22:33:48 -0400

    .

---
+++ Assets/TurnOffHand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TurnOffHand : MonoBehaviour
+{
+
+    public GameObject hand;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //(hand.GetComponent(Hand) as MonoBehaviour).enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
