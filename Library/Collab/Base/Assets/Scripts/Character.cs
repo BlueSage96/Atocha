1b36d5b42f59fe72628ade92f7e88ecaee619bba
Brittany Price <bprice0496@gmail.com>
2020-05-24 17:02:08 -0400

    .

---
+++ Library/Collab/Base/Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour{
+    private GameObject lemur;
+
+    // Start is called before the first frame update
+    void Start(){
+        lemur = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Animal")){
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+}
---
+++ Library/Collab/Download/Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour{
+    public Transform player;
+    public float moveSpeed = 5.0f;
+    private Rigidbody rb;
+    private Vector2 movement;
+   
+    // Start is called before the first frame update
+    void Start(){
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector3 direction = player.position - transform.position;
+        //Debug.Log(direction);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate(){
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction){
+        rb.MovePosition((Vector2) transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    //void OnTriggerEnter(Collider other){
+       // if (other.gameObject.CompareTag("Animal")){
+            //transform.Translate(Vector3.forward * Time.deltaTime);
+       // }
+    //}
+}
